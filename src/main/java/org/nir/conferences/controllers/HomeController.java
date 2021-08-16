71a2d6e29a9bdd213c2f1a00b2e1c4f0c757b43f
nir <niritzhak10@gmail.com>
2021-08-16 19:00:48 +0300

    Update the HomeController add the end point "/"

--- src/main/java/org/nir/conferences/controllers/HomeController.java
+++ src/main/java/org/nir/conferences/controllers/HomeController.java
@@ -1,10 +1,15 @@
 package org.nir.conferences.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController 
 {
-	
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Hello From Heroku";
+	}
   
 }
